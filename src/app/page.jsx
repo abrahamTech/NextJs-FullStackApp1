c0f7abfba2f9e80eb3a11674f47d57eca7a10056
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-07-06 21:23:10 -0600

    First screen from the home page has been finished

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,21 @@
-import Image from 'next/image';
-import styles from './page.module.css';
-import Hero from 'public/hero.png';
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "public/hero.png";
 
 export default function Home() {
   return (
-    <div>
-      {/* Import the image as a component, You don't need to write the width and height property, but you can asign width and height in a css class*/}
-      <Image src={Hero} alt='' className={styles.img} />
-    
-      {/* Import the image from URL
-      <Image  width={500} height={750} src="https://images.pexels.com/photos/879537/pexels-photo-879537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='Sculpture' /> */}
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products</h1>
+        <p className={styles.desc}>
+          Turning your idea into Reality. We bring together the teams from the global tech industry.
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        {/* Import the image as a component, You don't need to write the width and height property, but you can asign width and height in a css class*/}
+        <Image src={Hero} alt="" className={styles.img} />
+      </div>
     </div>
-  )
-};
+  );
+}
