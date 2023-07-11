2173e69d3917a0e6b021adb6c4bb1c4c9acaee8e
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-07-10 23:59:32 -0600

    Only the html code from About section has been added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
-import styles from "./page.module.css"
+import styles from "./page.module.css";
+import Image from 'next/image';
 
 const About = () => {
     return (
-        <div className={styles.wordColor}>About</div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image 
+                    src="https://images.pexels.com/photos/256381/pexels-photo-256381.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    fill={true} 
+                    alt='' 
+                    className={styles.img}    
+                />
+                <div className={styles.imgText}>
+                    <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+                    <h2 className={styles.imgDesc}>Handcrafting award winning digital experiences</h2>
+                </div>
+            </div>
+            <div className={styles.textContainer}>
+                <div className={styles.item}>
+                    <h1 className={styles.title}>Who Are We?</h1>
+                    <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error nisi rerum inventore soluta, atque cumque adipisci nobis vel, a magni neque. Blanditiis nobis culpa, modi officia consequuntur ducimus. Natus, omnis.
+                    <br /> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, adipisci corporis! Adipisci sit quos necessitatibus, facere blanditiis dignissimos odit suscipit animi nihil aliquam? Facilis dolor sint, eveniet deserunt ea impedit!</p>
+                </div>
+                <div className={styles.item}>
+                    <h1 className={styles.title}>What We Do?</h1>
+                    <p className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error nisi rerum inventore soluta. <br />Atque cumque adipisci nobis vel, a magni neque. Blanditiis nobis culpa, modi officia consequuntur ducimus. Natus, omnis.</p>
+                </div>
+            </div>
+        </div>
     )
 };
 
