7677f6ca403073d567d47811e0ed03c336aab265
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-07-11 23:09:23 -0600

    Contact section desgin and image's animations have been added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import styles from "./page.module.css";
+import Image from 'next/image';
+import Button from '@/components/Button/Button';
 
 const Contact = () => {
     return(
-        <div className={styles.wordColor}>Contact</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Let's Keep in Touch</h1>
+            <div className={styles.content}>
+                <div className={styles.imgContainer}>
+                    <Image 
+                        src="/contact.png"
+                        alt=""    
+                        fill={true}
+                        className={styles.image}
+                    />
+                </div>
+
+                <form action="" className={styles.form}>
+                    <input type="text" placeholder="Enter your Name" className={styles.input} />
+                    <input type="text" placeholder="Enter your Email" className={styles.input} />
+                    <textarea cols="30" rows="10" placeholder="Enter your Message" className={styles.textArea}></textarea>
+                    <Button url="#" text="Send" />
+                </form>
+            </div>
+        </div>
     )
 };
 
