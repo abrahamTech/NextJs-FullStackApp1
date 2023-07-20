be3704c91268331bafb424555cccb2da66e65ab7
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-07-20 00:00:03 -0600

    I have fetched data to Portfolio Category page from local JSON file

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,47 +1,48 @@
-import React from 'react';
+import React from "react";
 import styles from "./page.module.css";
 import Button from "@/components/Button/Button";
-import Image from 'next/image';
+import Image from "next/image";
 
-const Category = ({params}) => {
+import { items } from "./data";
+import { notFound } from "next/navigation";
+
+const getData = (cat) => {
+  const data = items[cat];
+
+  if (data) {
+    return data;
+  }
+
+  return notFound();
+};
+
+const Category = ({ params }) => {
+  console.log(params); //Category string from the URL
+  const data = getData(params.category);
+
   return (
     <div className={styles.container}>
-        <h1 className={styles.catTitle}>{params.category}</h1>
-        
-        <div className={styles.item}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+
+      {data.map((item) => (
+        <div className={styles.item} key={item.id}>
           <div className={styles.content}>
-            <h1 className={styles.title}>Test</h1>
-            <p className={styles.desc}>Desc</p>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
             <Button text="See More" url="#" />
           </div>
 
           <div className={styles.imgContainer}>
             <Image
-              className={styles.img} 
+              className={styles.img}
               fill={true}
-              src="https://images.pexels.com/photos/3224232/pexels-photo-3224232.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+              src={item.image}
             />
           </div>
         </div>
-
-        <div className={styles.item}>
-          <div className={styles.content}>
-            <h1 className={styles.title}>Test</h1>
-            <p className={styles.desc}>Desc</p>
-            <Button text="See More" url="#" />
-          </div>
-
-          <div className={styles.imgContainer}>
-            <Image
-              className={styles.img} 
-              fill={true}
-              src="https://images.pexels.com/photos/3224232/pexels-photo-3224232.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
-            />
-          </div>
-        </div>
-
+      ))};
     </div>
-  )
-}
+  );
+};
 
 export default Category;
