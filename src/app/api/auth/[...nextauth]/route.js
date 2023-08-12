c91f4e41ed8f8e2b82c1aec6fdac02ca8066470f
AbrahamGonzalez <131833001+abrahamTech@users.noreply.github.com>
2023-08-11 22:27:26 -0600

    feat(nextauth): add code for credentials authentication (Email and Password)

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,9 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connect from "@/utils/db";
+import bcrypt from "bcryptjs";
+import User from "@/models/User";
 
 const handler = NextAuth({
   providers: [
@@ -7,7 +11,40 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      async authorize(credentials){
+
+        await connect()
+
+        try {
+          //Check if the user exists in the DB or not
+          const user = await User.findOne({email: credentials.email});
+
+          if(user){
+            //Check password
+            const isPasswordCorrect = await bcrypt.compare( credentials.password, user.password);
+
+            if(isPasswordCorrect){
+              return user;
+            } else {
+              throw new Error("Wrong Credentials");
+            }
+
+          } else {
+            throw new Error("User not found!");
+          }
+
+        } catch (error) {
+          throw new Error(error)
+        }
+      }
+    }),
   ],
+  pages: {
+    error: "/dashboard/login",
+  },
 });
 
 export { handler as GET, handler as POST };
--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -5,8 +5,27 @@ import styles from "./page.module.css";
 import { signIn } from 'next-auth/react';
 
 const Login = () => {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    //Send the email and password
+    signIn("credentials", {email, password});
+  }
+
   return (
-    <div className={styles.wordColor}>
+    <div className={styles.container}>
+
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input type="email" className={styles.input} placeholder="Enter your email" required/>
+        <input type="password" className={styles.input} placeholder="Password" required/>
+
+        <button className={styles.button}>Login</button>
+      </form>
+
       <button onClick={() => signIn("google")}>Login with Google</button>
     </div>
   )
